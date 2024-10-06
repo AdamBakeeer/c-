5aa70150e180feca97439b300be6d16a12b33b06
adam bakeer <146668872+AdamBakeeer@users.noreply.github.com>
2024-10-06 20:15:50 +0000

    hiii

---
+++ mean.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void read_data(istream& input, vector <float>& data)
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        cerr << "Usage: ./mean <filename>" << endl;
+        return 1;
+    }
+
+    vector<float> data;
+
+    return 0;
+}
