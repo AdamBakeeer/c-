f44d63513471f917afecc706e8625adf9c56dbf3
adam bakeer <146668872+AdamBakeeer@users.noreply.github.com>
2024-10-09 16:13:14 +0000

    hhiiii

--- mean.cpp
+++ mean.cpp
@@ -6,7 +6,25 @@ using namespace std;
 
 void read_data(istream& input, vector <float>& data)
 {
+    float value;
+    while (input >> value) {
+        data.push_back(value);
+    }
 
+}
+
+float mean_value(const vector <float>& data)
+{
+    float total;
+    int size = data.size();
+
+    for (int i = 0; i<size; i++){
+        total = total + data[i];
+    }
+
+    float mean = total/size;
+
+    return mean;
 }
 
 int main(int argc, char* argv[])
@@ -15,8 +33,20 @@ int main(int argc, char* argv[])
         cerr << "Usage: ./mean <filename>" << endl;
         return 1;
     }
+    
+    ifstream infile(argv[1]);
+    if (not infile) {
+        cerr << "Error: cannot access " << argv[1] << endl;
+        return 2;
+    }
 
     vector<float> data;
 
-    return 0;
+    read_data(infile, data);
+
+    cout << data.size() << " values read from file" << endl;
+
+    cout << "Mean value = " << mean_value(data) << endl;
+
+    return 0; 
 }
